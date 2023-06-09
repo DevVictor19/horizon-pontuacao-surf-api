1adb94105b23c02f94a0192d027cef957e2636eb
DevVictor19 <antoniovictor12@live.com>
2023-06-09 09:04:59 -0300

    feat: adicionar configuração inicial typeORM

---
+++ src/database/data-source.ts
@@ -0,0 +1,20 @@
+import { DataSource } from 'typeorm';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.TYPEORM_HOST,
+  port: +process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  synchronize: true,
+  logging: false,
+  entities: [],
+  migrations: [],
+  subscribers: [],
+});
+
+export { AppDataSource };
