ac5217487b2ae7418ecdac80c61c54740b4ca50f
DevVictor19 <antoniovictor12@live.com>
2023-06-11 14:02:59 -0300

    feat(bateriasRoutes): adicionar rota vencedor

--- src/routes/baterias.routes.ts
+++ src/routes/baterias.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import { CreateBateriaController } from '../controllers/CreateBateriaController';
 import { GetAllBateriasController } from '../controllers/GetAllBateriasController';
+import { GetBateriaVencedorController } from '../controllers/GetBateriaVencedorController';
 
 const bateriasRoutes = Router();
 
 bateriasRoutes.post('/', new CreateBateriaController().handle);
 bateriasRoutes.get('/', new GetAllBateriasController().handle);
+bateriasRoutes.get('/:id/vencedor', new GetBateriaVencedorController().handle);
 
 export { bateriasRoutes };
