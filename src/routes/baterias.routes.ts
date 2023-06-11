864c456312770cd3e54932b5993b09a0b48e93c1
DevVictor19 <antoniovictor12@live.com>
2023-06-11 14:34:21 -0300

    feat(bateriasRoutes): adicionar middleware validar uuidv4

--- src/routes/baterias.routes.ts
+++ src/routes/baterias.routes.ts
@@ -4,10 +4,17 @@ import { CreateBateriaController } from '../controllers/CreateBateriaController'
 import { GetAllBateriasController } from '../controllers/GetAllBateriasController';
 import { GetBateriaVencedorController } from '../controllers/GetBateriaVencedorController';
 
+import { validateUuidV4Params } from '../middlewares/validateUuidV4Params';
+
 const bateriasRoutes = Router();
 
 bateriasRoutes.post('/', new CreateBateriaController().handle);
 bateriasRoutes.get('/', new GetAllBateriasController().handle);
-bateriasRoutes.get('/:id/vencedor', new GetBateriaVencedorController().handle);
+
+bateriasRoutes.get(
+  '/:id/vencedor',
+  validateUuidV4Params,
+  new GetBateriaVencedorController().handle
+);
 
 export { bateriasRoutes };
