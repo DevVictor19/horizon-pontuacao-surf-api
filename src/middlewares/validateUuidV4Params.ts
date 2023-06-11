12d5d59bebd98a4ae87e8f73c17e05950358ea00
DevVictor19 <antoniovictor12@live.com>
2023-06-11 14:33:55 -0300

    feat(validateUuidV4Params): implementar middleware

---
+++ src/middlewares/validateUuidV4Params.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+
+export function validateUuidV4Params(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const { id } = request.params;
+
+  const uuid_v4_regex =
+    /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
+
+  const isValid_uuid = uuid_v4_regex.test(id);
+
+  if (!isValid_uuid) {
+    return response
+      .status(400)
+      .json({ mensagem: 'O parâmetro de busca de ser um uuidV4 válido' });
+  }
+
+  next();
+}
