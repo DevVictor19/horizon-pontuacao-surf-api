556082c3de77a499d2788b3f894471ec85695c37
DevVictor19 <antoniovictor12@live.com>
2023-06-12 10:03:51 -0300

    fix(surfistasParams): adicionar validation fn

--- src/validations/surfistasParams.validations.ts
+++ src/validations/surfistasParams.validations.ts
@@ -1,9 +1,8 @@
 import { ValidateParamsSchema } from '../middlewares/ValidateParams';
-import { isNumber } from '../utils/isNumber';
 
 export const surfistasParamsValidationSchema: ValidateParamsSchema = {
   id: {
     errorMessage: 'Parâmetro de pesquisa id inválido',
-    validationFn: isNumber,
+    validationFn: (value: string) => !isNaN(+value),
   },
 };
