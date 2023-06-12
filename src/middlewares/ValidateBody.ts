13ce51db87270f27d9aa0b0d4395b5a7e3f447b3
DevVictor19 <antoniovictor12@live.com>
2023-06-12 09:17:39 -0300

    feat(ValidateBody): implementar middleware

---
+++ src/middlewares/ValidateBody.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from 'express';
+
+type Rules = {
+  errorMessage: string;
+  validationFn: (value: any) => boolean;
+};
+
+export type ValidateBodySchema = {
+  [key: string]: Rules;
+};
+
+class ValidateBody {
+  validate(schema: ValidateBodySchema) {
+    return function (request: Request, response: Response, next: NextFunction) {
+      const { body } = request;
+
+      const errors: string[] = [];
+
+      Object.keys(schema).forEach((item) => {
+        const bodyItem = body[item];
+
+        const isValid = schema[item].validationFn(bodyItem);
+
+        if (!isValid) {
+          errors.push(schema[item].errorMessage);
+        }
+      });
+
+      if (errors.length > 0) {
+        return response.status(400).json({ erros: errors });
+      }
+
+      return next();
+    };
+  }
+}
+
+export { ValidateBody };
